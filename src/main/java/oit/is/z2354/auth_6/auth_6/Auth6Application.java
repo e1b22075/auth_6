593e952ed5e14f7b9e229ffed756513595c91ff9
hirao mitsuki <e1b22075@oit.ac.jp>
2024-10-08 13:55:43 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2354/auth_6/auth_6/Auth6Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2354.auth_6.auth_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth6Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2354/auth_6/auth_6/Auth6ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2354.auth_6.auth_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
